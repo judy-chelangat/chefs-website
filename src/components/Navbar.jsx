698737a05fd65ae45aa98bb272d4ed412c563cd9
judy-chelangat <judysigilai75@gmail.com>
2024-01-08 16:44:30 +0300

    added usestate effect for color change and event listener

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Navbar.css'
+import {Link } from 'react-router-dom'
+import {FaBars,FaTimes} from 'react-icons/fa'
 
 function Navbar() {
+    const [click,setClick] =useState(false)
+    const handleClick=()=> setClick(!click)
+
+    const [color,setColor]=useState(false)
+
+    const changeColor=()=>{
+        if(window.scrollY >=100){
+            setColor(true)
+        }
+        else{
+            setColor(false)
+        }
+    }
+    window.addEventListener('scroll',changeColor)
   return (
     <div>
       
