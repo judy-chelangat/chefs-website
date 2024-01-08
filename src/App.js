91af155ee83cc521a9e6afa1b48e59dcf8856849
judy-chelangat <judysigilai75@gmail.com>
2024-01-08 16:14:33 +0300

    added the exact path to the routes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import About from './routes/About';
 import Contact from './routes/Contact';
 import Services from './routes/Services';
 import Home from './routes/Home';
+import {Route,Routes} from 'react-router-dom'
 
 function App() {
   return (
     <>
-
+    <Routes>
+      <Route exact path ='/' element={<Home/>}/>
+      <Route path ='/contacts' element={<Contact/>}/>
+      <Route path ='/about' element={<About/>}/>
+      <Route path ='/services' element={<Services/>}/>
+    </Routes>
     </>
   );
 }
